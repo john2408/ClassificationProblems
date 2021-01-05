51c8942ce89f96837b6fcef4669c97d785ff5543
john2408 <john.torres.developer@outlook.com>
2021-01-05 21:59:08 +0100

    yml file adjusted for config params

--- Sentiment_Analysis/main.py
+++ Sentiment_Analysis/main.py
@@ -14,6 +14,7 @@ from sys import path
 path.append(  join( getcwd() , 'functions/' ) )
 
 from functions import preprocessing, modelling, postprocessing
+from config import ConfigDict
 
 
 # Run only for the first time#
@@ -174,56 +175,43 @@ if __name__ == "__main__":
     # ----------------------------------------------------------------#
 
 
-    #config = ConfigDict.read('config/config_param.yml')
-
+    config = ConfigDict.read('config/config_param.yml')
     current_time = datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
 
-    data_dir = join( getcwd() , 'data/SA_4_Categories.csv' ) 
-
-    sent_tokenizer = False # TODO: Adjust for input to CNN
+    sent_tokenizer = config['params']['tokenization_options']['sent_tokenizer'] # TODO: Adjust for input to CNN
 
     # Text Cleaning Options
-    use_nltk_cleaning = False
-    text_cleaning = True
+    use_nltk_cleaning = config['params']['tokenization_options']['use_nltk_cleaning']
+    text_cleaning = config['params']['tokenization_options']['text_cleaning']
 
     # Word Tokenizer Options
-    use_tfidf_tokenizer = False # TODO: Adjust for input to CNN
-    use_keras_tokenizer = True
+    use_tfidf_tokenizer = config['params']['tokenization_options']['use_tfidf_tokenizer'] # TODO: Adjust for input to CNN
+    use_keras_tokenizer = config['params']['tokenization_options']['use_keras_tokenizer']
 
     # If set to FALSE then keras embedding space training is used instead
     # Embedding Space possibilites are GloVe or TFIDF
-    use_pretrained_embeddings = True
+    use_pretrained_embeddings = config['params']['tokenization_options']['use_pretrained_embeddings']
 
     # Only if use_pretrained_embeddings == True then select embedding vector space type
-    use_glove_pretrained_embeddings_weights = False
-    use_tfidf_as_embedding_weights = True
+    use_glove_pretrained_embeddings_weights = config['params']['tokenization_options']['use_glove_pretrained_embeddings_weights']
+    use_tfidf_as_embedding_weights = config['params']['tokenization_options']['use_tfidf_as_embedding_weights']
 
     # Dictionary which will cotain all the model's variables
     data = {}
 
     # Initialize Model
-    data['epochs'] = 30 # NO. of optimizatoin runs
-    data['batch_size'] = 16 # No. of sentences batch to train
-    data['num_words'] = 5000 # No. of words to use in the embedding space of GloVe or TFIDF
-    data['cv'] = 4 # No. of Cross Validations
-    data['n_iter'] = 5 # No. of Iterations
-    data['seq_input_len'] = 40 # Length of the vector sentence ( no. of words per sentence)
-    data['embedding_dim'] = 40 # Length of the word vector ( dimension in the embedding space)
-    data['nodes_hidden_dense_layer'] = 5 # No. of nodes for hidden Dense layer
+    data['epochs'] = config['params']['data']['epochs'] # NO. of optimizatoin runs
+    data['batch_size'] = config['params']['data']['batch_size'] # No. of sentences batch to train
+    data['num_words'] = config['params']['data']['num_words'] # No. of words to use in the embedding space of GloVe or TFIDF
+    data['cv'] = config['params']['data']['cv'] # No. of Cross Validations
+    data['n_iter'] = config['params']['data']['n_iter'] # No. of Iterations
+    data['seq_input_len'] = config['params']['data']['seq_input_len'] # Length of the vector sentence ( no. of words per sentence)
+    data['embedding_dim'] = config['params']['data']['embedding_dim'] # Length of the word vector ( dimension in the embedding space)
+    data['nodes_hidden_dense_layer'] = config['params']['data']['nodes_hidden_dense_layer'] # No. of nodes for hidden Dense layer
 
 
-    data['filepath'] = 'D:/Semillero Data Science/Deep Learning/pre-trained Word Embeddings/GloVe/glove.6B.50d.txt' # File path to GLoVe pretrained embedding words
+    data['filepath'] = config['params']['data']['filepath'] # File path to GLoVe pretrained embedding words
     data['output_file'] = f"results/{current_time}_Result.txt" # Name of output result file
-
-
-    param_grid = dict(num_filters_cv = [(64,16), (64,32), (128,16), (128,32), (256,64), (256,32), (256,64), (512,128), (512, 32)], # No of filter to use in convolution
-                    kernel_size_cv = [(2,3), (2,4), (3,4), (3,5)], # No of words to check per Convolution 
-                    vocab_size = [3000, 4000, 5000, 6000], # Vocab size if keras embedding space training is wanted
-                    embedding_dim = [20, 30, 40, 50], 
-                    seq_input_len = [50, 40, 30, 20, 10], 
-                    nodes_hidden_dense_layer = [5, 10, 15, 20, 40],
-                    use_pretrained_embeddings = [True, False])
-
 
     # Small Test
     param_grid = dict(num_filters_cv = [(64,16)],
@@ -236,7 +224,7 @@ if __name__ == "__main__":
 
 
     # Preprocess data
-    data, param_grid = preprocess(data_dir = data_dir, 
+    data, param_grid = preprocess(data_dir = config['params']['input_data']['5_labels'], 
                                 data = data, 
                                 param_grid = param_grid)
 
